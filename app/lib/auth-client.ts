361f9d2aa3a56d1b9ed0f779f4b86f5f93691178
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-06-21 17:21:06 +0700

    21

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/vue";
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
 });
