9e472fadd0713da26818dde196db0fae39e41d17
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-06-23 16:51:24 +0700

    32

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,5 +1,25 @@
 <template>
-    <h1 class="text-4xl">
-        Hello, <span class="font-bold underline">World</span>!
-    </h1>
+    <UiFlipCard>
+        <template #default>
+            <img
+                src="https://random.imagecdn.app/640/480"
+                alt="Image"
+                class="size-full rounded-2xl object-cover shadow-2xl shadow-black/40"
+            />
+            <div class="absolute bottom-4 left-4 text-xl font-bold">
+                Что это за сайт?
+            </div>
+        </template>
+        <template #back>
+            <div class="flex min-h-full flex-col gap-2">
+                <h1 class="text-xl font-bold">Inspira UI</h1>
+                <p
+                    class="mt-1 border-t py-4 text-base leading-normal font-medium"
+                >
+                    Это сайт YaroSheyh. Его написал YaroSheyh. Использую Nuxt 4,
+                    Better Auth с базой данных в Turso (sqlite, libSQL).
+                </p>
+            </div>
+        </template>
+    </UiFlipCard>
 </template>
