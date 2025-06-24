8028f4601b64cea5f639e67caed61571b484e149
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-06-24 11:04:10 +0700

    44

--- app/components/HeaderPart.vue
+++ app/components/HeaderPart.vue
@@ -1,7 +1,10 @@
 <template>
     <header>
         <nav class="py-8">
-            <ul class="flex h-0 items-center justify-center text-2xl font-bold">
+            <ul
+                v-if="$device.isDesktopOrTablet"
+                class="flex h-0 items-center justify-center text-2xl font-bold"
+            >
                 <li class="after:m-2 after:content-['|']">
                     <NuxtLink to="/">На главную</NuxtLink>
                 </li>
@@ -12,6 +15,26 @@
                     <NuxtLink to="/sign-in">Войти</NuxtLink>
                 </li>
                 <li class="after:m-2 after:content-['|']">
+                    <NuxtLink to="/sign-up">Зарегистрироваться</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/dashboard">Панель</NuxtLink>
+                </li>
+            </ul>
+            <ul
+                v-if="$device.isMobile"
+                class="flex flex-col items-center justify-center text-2xl font-bold"
+            >
+                <li class="after:m-2 after:content-['-']">
+                    <NuxtLink to="/">На главную</NuxtLink>
+                </li>
+                <li class="after:m-2 after:content-['-']">
+                    <NuxtLink to="/about">О сайте</NuxtLink>
+                </li>
+                <li class="after:m-2 after:content-['-']">
+                    <NuxtLink to="/sign-in">Войти</NuxtLink>
+                </li>
+                <li class="after:m-2 after:content-['-']">
                     <NuxtLink to="/sign-up">Зарегистрироваться</NuxtLink>
                 </li>
                 <li>
