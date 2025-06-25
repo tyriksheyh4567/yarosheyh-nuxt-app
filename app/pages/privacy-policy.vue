e7d7799afee93ce28384965e245f221c03f00320
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-06-25 17:11:49 +0700

    97

--- app/pages/privacy-policy.vue
+++ app/pages/privacy-policy.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="size-1/2 rounded-sm border-0 bg-blue-200 p-8">
         <article class="prose lg:prose-xl">
-            <h1>{{ $t("privacy-policy") }}</h1>
-            <p>
-                {{ $t("privacy-policy-p-1") }}
-            </p>
-            <p>{{ $t("privacy-policy-p-2") }}</p>
+            <h1>Политика конфедициальности</h1>
+            <p>Вы соглашаетесь с принятием куки для работы аутентификации.</p>
+            <p>Я уважаю каждого пользователя моего веб-сайта.</p>
         </article>
     </div>
 </template>
